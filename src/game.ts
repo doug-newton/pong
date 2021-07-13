7404da3989cca7dde4b73d8cb919a3f1602e914c
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-13 21:04:00 +0200

    add second paddle

--- src/game.ts
+++ src/game.ts
@@ -4,10 +4,15 @@ import { Paddle } from "./paddle";
 
 export class Game extends BaseGame {
 
+    paddleTop: Paddle = new Paddle()
+    paddleBottom: Paddle = new Paddle()
+
     constructor() {
         super()
         this.registerGameObject(new Ball())
-        this.registerGameObject(new Paddle())
+        this.paddleTop.pos.y = 50;
+        this.registerGameObject(this.paddleBottom)
+        this.registerGameObject(this.paddleTop)
     }
 
 }
