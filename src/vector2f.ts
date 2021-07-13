087bf381fb6c44aa3612ae5a566cdc7a51fb9060
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-13 20:23:09 +0200

    use vector2f for x/y or w/h

--- src/vector2f.ts
+++ src/vector2f.ts
@@ -1,4 +1,25 @@
 export class Vector2f {
+    constructor(x: number = 0, y: number = 0) {
+        this.x = x
+        this.y = y
+    }
     x: number = 0
     y: number = 0
+    get w(): number {
+        return this.x
+    }
+    set w(w: number) {
+        this.x = w
+    }
+    get h(): number {
+        return this.y
+    }
+    set h(h: number) {
+        this.y = h
+    }
+    set(v: Vector2f) {
+        console.log('set called')
+        this.x = v.x
+        this.y = v.y
+    }
 }
