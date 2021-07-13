922815cad44eab24a7c83040148a178b7daf70df
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-13 20:48:43 +0200

    register game as parent of game objects

---
+++ src/base-game.ts
@@ -0,0 +1,66 @@
+import { Canvas } from "./canvas";
+import { GameObject } from "./game-object";
+import { IGame } from "./i-game";
+
+export class BaseGame implements IGame {
+
+    protected registerGameObject(o: GameObject){
+        this.gameObjects.push(o);
+        o.onRegister(this)
+    }
+
+    protected init() {
+        this.registerEventListeners()
+    }
+
+    public getCanvasObject(): Canvas {
+        return this.canvas
+    }
+
+    protected canvas: Canvas = new Canvas()
+    protected gameObjects: GameObject[] = []
+
+    protected registerEventListeners() {
+        document.addEventListener('mousedown', (ev: MouseEvent) => this.onMouseDown(ev));
+        document.addEventListener('mouseup', (ev: MouseEvent) => this.onMouseUp(ev));
+        document.addEventListener('mousemove', (ev: MouseEvent) => this.onMouseMove(ev));
+        document.addEventListener('contextmenu', e => {
+            e.preventDefault();
+            return false;
+        })
+    }
+
+    protected onMouseDown(event: MouseEvent): void{
+        this.gameObjects.forEach(o => o.onMouseDown(event));
+    }
+
+    protected onMouseUp(event: MouseEvent): void{
+        this.gameObjects.forEach(o => o.onMouseUp(event));
+    }
+
+    protected onMouseMove(event: MouseEvent): void{
+        this.gameObjects.forEach(o => o.onMouseMove(event));
+    }
+
+    protected draw(): void {
+        this.canvas.clear()
+        this.gameObjects.forEach(o => o.draw(this.canvas.context));
+    }
+
+    protected update() {
+        this.gameObjects.forEach(o => o.update());
+    }
+
+    protected gameLoop() {
+        this.update();
+        this.draw();
+        window.requestAnimationFrame(() => {
+            this.gameLoop();
+        })
+    }
+
+    public run(){
+        this.init();
+        this.gameLoop();
+    }
+}
--- src/game.ts
+++ src/game.ts
@@ -1,64 +1,13 @@
 import { Ball } from "./ball";
-import { Canvas } from "./canvas";
-import { GameObject } from "./game-object";
+import { BaseGame } from "./base-game";
 import { Paddle } from "./paddle";
 
-export class Game {
+export class Game extends BaseGame {
 
     constructor() {
-        this.gameObjects.push(new Ball())
-        this.gameObjects.push(new Paddle())
-    }
-
-    private init() {
-        this.registerEventListeners()
-    }
-
-    private canvas: Canvas = new Canvas()
-    private gameObjects: GameObject[] = []
-
-    private registerEventListeners() {
-        document.addEventListener('mousedown', (ev: MouseEvent) => this.onMouseDown(ev));
-        document.addEventListener('mouseup', (ev: MouseEvent) => this.onMouseUp(ev));
-        document.addEventListener('mousemove', (ev: MouseEvent) => this.onMouseMove(ev));
-        document.addEventListener('contextmenu', e => {
-            e.preventDefault();
-            return false;
-        })
-    }
-
-    private onMouseDown(event: MouseEvent): void{
-        this.gameObjects.forEach(o => o.onMouseDown(event));
-    }
-
-    private onMouseUp(event: MouseEvent): void{
-        this.gameObjects.forEach(o => o.onMouseUp(event));
-    }
-
-    private onMouseMove(event: MouseEvent): void{
-        this.gameObjects.forEach(o => o.onMouseMove(event));
-    }
-
-    private draw(): void {
-        this.canvas.clear()
-        this.gameObjects.forEach(o => o.draw(this.canvas.context));
-    }
-
-    private update() {
-        this.gameObjects.forEach(o => o.update());
-    }
-
-    private gameLoop() {
-        this.update();
-        this.draw();
-        window.requestAnimationFrame(() => {
-            this.gameLoop();
-        })
-    }
-
-    run(){
-        this.init();
-        this.gameLoop();
+        super()
+        this.registerGameObject(new Ball())
+        this.registerGameObject(new Paddle())
     }
 
 }
