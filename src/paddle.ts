6f2b42b6749c3a98bb8269e4797b5133e5b54737
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-13 20:57:01 +0200

    align middle x of paddle to cursor and style

--- src/paddle.ts
+++ src/paddle.ts
@@ -1,6 +1,8 @@
 import { Canvas } from "./canvas";
 import { GameObject } from "./game-object";
+import { GameSettings } from "./game-settings";
 import { RenderUtil } from "./render-util";
+import { Style } from "./style";
 import { Vector2f } from "./vector2f";
 
 export class Paddle extends GameObject {
@@ -8,14 +10,17 @@ export class Paddle extends GameObject {
     constructor() {
         super()
         this.pos = new Vector2f(0,0);
-        this.dim = new Vector2f(100, 20);
+        this.dim = new Vector2f(200, 20);
+        this.pos.y = GameSettings.canvasDimensions.h - 50
     }
+
+    style: Style = new Style("white", "cyan", 1, true, true);
 
     pos: Vector2f = new Vector2f()
     dim: Vector2f = new Vector2f()
 
     public draw(context: CanvasRenderingContext2D): void {
-        RenderUtil.drawRect(context, this.pos.x, this.pos.y, this.dim.w, this.dim.h)
+        RenderUtil.drawRect(context, this.pos.x, this.pos.y, this.dim.w, this.dim.h, this.style)
     }
 
     public update(): void {
@@ -23,11 +28,7 @@ export class Paddle extends GameObject {
 
     public onMouseMove(event: MouseEvent) {
         let canvas: Canvas = this.getParent().getCanvasObject()
-        let cursor = new Vector2f()
-        cursor.x = (event.pageX - canvas.canvas.offsetLeft)
-        cursor.y = (event.pageY - canvas.canvas.offsetTop)
-        this.pos.x = cursor.x
-        this.pos.y = cursor.y
+        this.pos.x = (event.pageX - canvas.canvas.offsetLeft) - this.dim.w / 2
     }
 
 }
