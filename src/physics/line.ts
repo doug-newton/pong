f807f72d5ef045a312b8eeccbadff3eeac240d21
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-20 19:13:07 +0200

    delete shitty wip

--- src/physics/line.ts
+++
@@ -1,6 +0,0 @@
-export class Line {
-    gradient: number | null = null
-    yIntercept: number | null = null;
-    //    only used if the gradient is infinity
-    xIntercept: number | null = null;
-}
