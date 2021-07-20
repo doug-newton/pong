4812ae053f2733c2408042913a7ba2f275b7ac74
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-20 19:32:44 +0200

    calculate gradient from angle

--- src/geometry/geometry.ts
+++ src/geometry/geometry.ts
@@ -1,5 +1,14 @@
 class Geometry_Singleton {
 
+    private notInfinity: number = 16331239353195370
+
+    public getGradientFromAngle(theta: number): number {
+        let result: number = Math.tan(theta)
+        if (result == this.notInfinity)
+            result = Infinity
+        return result
+    }
+
 }
 
 export const Geometry: Geometry_Singleton = new Geometry_Singleton();
--- tests/geometry.ts
+++ tests/geometry.ts
@@ -1,4 +1,33 @@
 import { expect } from 'chai';
+import { Geometry } from '../src/geometry/geometry';
 
-describe('geometry', ()=>{
+describe('geometry', () => {
+    let error: number = 0.09
+
+    it('can calculate the gradient from an angle', () => {
+        let theta: number = Math.PI / 6
+        let m: number = Geometry.getGradientFromAngle(theta)
+        expect(Math.abs(m - 1 / 2)).to.be.lessThan(error)
+
+        theta = 0
+        m = Geometry.getGradientFromAngle(theta)
+        expect(m).to.equal(0)
+
+        theta = Math.PI / 2
+        m = Geometry.getGradientFromAngle(theta)
+        expect(m).to.equal(Infinity)
+
+        theta = Math.PI
+        m = Geometry.getGradientFromAngle(theta)
+        expect(Math.abs(m - 0)).to.be.lessThan(error)
+
+        theta = Math.PI * 2
+        m = Geometry.getGradientFromAngle(theta)
+        expect(Math.abs(m - 0)).to.be.lessThan(error)
+
+        theta = Math.PI / 4
+        m = Geometry.getGradientFromAngle(theta)
+        expect(m * m - 2).to.be.lessThan(error)
+    })
+
 })
