df2daa5645965201e587ebea95a19ec109df9067
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-20 20:07:33 +0200

    add line intersection

--- src/geometry/geometry.ts
+++ src/geometry/geometry.ts
@@ -1,3 +1,30 @@
+import { Vector2f } from "../vector2f"
+
+let ex1: string = 'vertical lines must define an x-intercept'
+let ex2: string = 'non-vertical lines must define a y-intercept'
+let ex3: string = 'vertical lines cannot have a y-intercept'
+let ex4: string = 'horizontal lines cannot have an x-intercept'
+
+export class Line {
+
+    yint: number | null = 0
+    xint: number | null = 0
+    m: number = 0
+
+    constructor(m: number, xint: number | null, yint: number | null) {
+        if (m == Infinity) {
+            if (xint == null) throw ex1
+            if (yint != null) throw ex3
+        }
+        else if (m == 0 && xint != null) throw ex4
+        else if (yint == null) throw ex2
+
+        this.m = m
+        this.xint = xint
+        this.yint = yint
+    }
+}
+
 class Geometry_Singleton {
 
     private notInfinity: number = 16331239353195370
@@ -7,6 +34,34 @@ class Geometry_Singleton {
         if (result == this.notInfinity)
             result = Infinity
         return result
+    }
+
+    public getLineIntersection(line1: Line, line2: Line): Vector2f | null {
+
+        if (line1.m == line2.m) return null
+
+        let x: number
+        let y: number
+
+        if (this.isVertical(line1)) {
+            x = line1.xint!
+            y = line2.m * x + line2.yint!
+            return new Vector2f(x, y)
+        }
+        else if (this.isVertical(line2)) {
+            x = line2.xint!
+            y = line1.m * x + line1.yint!
+            return new Vector2f(x, y)
+        }
+        else {
+            x = -(line1.yint! - line2.yint!) / (line1.m - line2.m)
+            y = line1.m * x + line1.yint!
+            return new Vector2f(x, y)
+        }
+    }
+
+    private isVertical(line: Line): boolean {
+        return line.m == Infinity
     }
 
 }
--- tests/geometry.ts
+++ tests/geometry.ts
@@ -1,10 +1,11 @@
 import { expect } from 'chai';
-import { Geometry } from '../src/geometry/geometry';
+import { Geometry, Line } from '../src/geometry/geometry';
+import { Vector2f } from '../src/vector2f';
 
 describe('geometry', () => {
     let error: number = 0.09
 
-    it('can calculate the gradient from an angle', () => {
+    it('gradient can be calculated from angle', () => {
         let theta: number = Math.PI / 6
         let m: number = Geometry.getGradientFromAngle(theta)
         expect(Math.abs(m - 1 / 2)).to.be.lessThan(error)
@@ -28,6 +29,37 @@ describe('geometry', () => {
         theta = Math.PI / 4
         m = Geometry.getGradientFromAngle(theta)
         expect(m * m - 2).to.be.lessThan(error)
+    })
+
+    it('parallel lines dont intersect', () => {
+        let a: Line = new Line(2, null, 3)
+        let b: Line = new Line(2, null, 4)
+
+        let n: Vector2f | null = Geometry.getLineIntersection(a, b)
+        expect(n).to.deep.equal(null)
+
+        n = Geometry.getLineIntersection(b, a)
+        expect(n).to.deep.equal(null)
+    })
+
+    it('vertical lines intersect with non-vertical lines', () => {
+        let a: Line = new Line(Infinity, 3, null)
+        let b: Line = new Line(2, null, 4)
+
+        let n: Vector2f | null = Geometry.getLineIntersection(a, b)
+        expect(n).to.deep.equal(new Vector2f(3, 10))
+        n = Geometry.getLineIntersection(b, a)
+        expect(n).to.deep.equal(new Vector2f(3, 10))
+    })
+
+    it('non-parallel, non-vertical lines intersect', () => {
+        let a: Line = new Line(1, null, 3)
+        let b: Line = new Line(2, null, 1)
+
+        let n: Vector2f | null = Geometry.getLineIntersection(a, b)
+        expect(n).to.deep.equal(new Vector2f(2, 5))
+        n = Geometry.getLineIntersection(b, a)
+        expect(n).to.deep.equal(new Vector2f(2, 5))
     })
 
 })
