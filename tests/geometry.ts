f016a2bc91970ad88ae7358b46fde89677daeb2c
Doug <27059467+doug-newton@users.noreply.github.com>
2021-07-20 22:07:39 +0200

    relabel geometry tests

--- tests/geometry.ts
+++ tests/geometry.ts
@@ -5,7 +5,7 @@ import { Vector2f } from '../src/vector2f';
 describe('geometry', () => {
     let delta: number = 0.00001
 
-    it('gradient can be calculated from angle', () => {
+    it('creates gradients from angles', () => {
         let theta: number 
         let m: number 
 
@@ -22,7 +22,7 @@ describe('geometry', () => {
         expect(m).to.be.approximately(1, delta)
     })
 
-    it('parallel lines dont intersect', () => {
+    it('doesn\'t intersect parallel lines', () => {
         let a: Line = new Line(2, null, 3)
         let b: Line = new Line(2, null, 4)
 
@@ -33,7 +33,7 @@ describe('geometry', () => {
         expect(n).to.deep.equal(null)
     })
 
-    it('vertical lines intersect with non-vertical lines', () => {
+    it('intersects vertical lines with non-vertical lines', () => {
         let a: Line = new Line(Infinity, 3, null)
         let b: Line = new Line(2, null, 4)
 
@@ -43,7 +43,7 @@ describe('geometry', () => {
         expect(n).to.deep.equal(new Vector2f(3, 10))
     })
 
-    it('non-parallel, non-vertical lines intersect', () => {
+    it('intersects non-parallel, non-vertical lines', () => {
         let a: Line = new Line(1, null, 3)
         let b: Line = new Line(2, null, 1)
 
@@ -53,7 +53,7 @@ describe('geometry', () => {
         expect(n).to.deep.equal(new Vector2f(2, 5))
     })
 
-    it('can create line from ray', () => {
+    it('creates lines from rays', () => {
         let ray: Ray = new Ray(2, 6, 0.463647609)
         let line: Line = Geometry.lineFromRay(ray);
         let expected: Line = new Line(1 / 2, null, 5)
@@ -63,25 +63,25 @@ describe('geometry', () => {
         expect(line.yint).to.be.approximately(expected.yint!, delta)
     })
 
-    it('can create non-vertical, non-horizontal line from line segment', () => {
+    it('creates non-vertical, non-horizontal lines from non-vertical, non-horizontal line segments', () => {
         let lineSegment: LineSegment = new LineSegment(0, 0, 5, 10)
         let line: Line | null = Geometry.lineFromLineSegment(lineSegment)
         expect(line).to.deep.equal(new Line(2, null, 0))
     })
 
-    it('can create vertical line from line segment', () => {
+    it('creates vertical lines from vertical line segments', () => {
         let lineSegment: LineSegment = new LineSegment(0, 0, 0, 5)
         let line: Line | null = Geometry.lineFromLineSegment(lineSegment)
         expect(line).to.deep.equal(new Line(Infinity, 0, null))
     })
 
-    it('can create horizontal line from line segment', () => {
+    it('creates horizontal lines from horizontal line segments', () => {
         let lineSegment: LineSegment = new LineSegment(0, 5, 10, 5)
         let line: Line | null = Geometry.lineFromLineSegment(lineSegment)
         expect(line).to.deep.equal(new Line(0, null, 5))
     })
 
-    it('doesn\'t create line from single-point line segment', () => {
+    it('doesn\'t create lines from single-point line segments', () => {
         let lineSegment: LineSegment = new LineSegment(1, 0, 1, 0)
         let line: Line | null = Geometry.lineFromLineSegment(lineSegment)
         expect(line).to.equal(null)
