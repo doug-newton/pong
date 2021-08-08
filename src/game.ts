2bf20edf255b19eafad7576686697e0b3e73796f
Doug <27059467+doug-newton@users.noreply.github.com>
2021-08-08 17:08:22 +0200

    delete tank, bullet, laser

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,9 @@
 import { BaseGame } from "./base-game";
-import { Tank } from "./game-objects/tank";
 
 export class Game extends BaseGame {
 
-    tank: Tank = new Tank()
-
     constructor() {
         super()
-        this.registerGameObject(this.tank)
     }
 
 }
